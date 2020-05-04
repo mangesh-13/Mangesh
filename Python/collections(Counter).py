e9820a1036c599eb132500b489d823176bb7d926
mangesh-13 <mangeshh902@gmail.com>
2020-05-04 23:44:00 +0530

    item(), values(), keys() methods added

--- Python/collections(Counter).py
+++ Python/collections(Counter).py
@@ -9,3 +9,22 @@ from collections import Counter
 a="aaaaaaabbbbbbcccc"
 my_counter=Counter(a)
 print(my_counter)
+
+
+'''
+Definition and Usage
+The items() method returns a view object. The view object contains the key-value pairs of the dictionary,
+ as tuples in a list.
+
+The view object will reflect any changes done to the dictionary.
+
+Syntax
+dictionary.items()
+Parameter Values:
+No parameters
+'''
+
+print(my_counter.items())
+print(my_counter.values())
+print(my_counter.keys())
+ 
