835fdc4e364200ff05cee1278e57b72b428868ec
mangesh-13 <mangeshh902@gmail.com>
2020-05-04 22:13:37 +0530

    Counter

---
+++ Python/collections(Counter).py
@@ -0,0 +1,11 @@
+#collection: Counter
+
+
+# Counter is containor that stores the elements as dictionary keys and
+# their counts as dictionary values
+
+
+from collections import Counter
+a="aaaaaaabbbbbbcccc"
+my_counter=Counter(a)
+print(my_counter)
