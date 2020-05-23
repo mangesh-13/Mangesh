9fff5ef6f4dd172e28181ea4ab46857ce209818f
mangesh-13 <mangeshh902@gmail.com>
2020-05-24 01:30:26 +0530

    deleted

--- Python_Day2/Q1.py
+++
@@ -1,8 +0,0 @@
-sum=0
-n=int(input("Enter n:"))
-for i in range(1,n+1):
-     print(i," + " ,end="")
-     sum+=(i)
-print("=",sum)
-
-
--- Python_Day2/Q3.py
+++
@@ -1,5 +0,0 @@
-'''3.	Write a Python program to create the multiplication table (from 1 to 10) of a number. '''
-n=int(input("Enter number who's multiplication table you want:"))
-x=n
-for i in range(1,11):
-    print(i*n,end=" ")
